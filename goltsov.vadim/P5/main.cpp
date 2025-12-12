06a218a4132692cd6e0425c0ced0f162fcd56d09
vadik <vadik@debian>
2025-12-12 03:40:05 +0300

    Add Rubber

--- goltsov.vadim/P5/main.cpp
+++ goltsov.vadim/P5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 namespace goltsov
 {
@@ -24,18 +25,20 @@ namespace goltsov
   {
     Rectangle(double width, double height, point_t pos)
     {
+      if (width <= 0 || height <= 0)
+      {
+        throw std::logic_error("The width and height must be greater than zero.");
+      }
       a.width = width;
       a.height = height;
       a.pos = pos;
     }
-    Rectangle(const Rectangle& other)
-    {
-      a = other.a;
-    }
-    Rectangle(Rectangle&& other)
-    {
-      a = std::move(other.a);
-    }
+    Rectangle(const Rectangle& other):
+      a(other.a)
+    {}
+    Rectangle(Rectangle&& other):
+      a(std::move(other.a))
+    {}
     Rectangle& operator=(const Rectangle& other)
     {
       if (this != &other)
@@ -60,6 +63,62 @@ namespace goltsov
     void scale(double k) override;
   private:
       rectangle_t a;
+  };
+  struct Rubber : Shape
+  {
+    Rubber(double r1, point_t pos1, double r2, point_t pos2):
+      r1(r1), r2(r2), pos1(pos1), pos2(pos2)
+    {
+      if (r1 <= 0 || r2 <= 0)
+      {
+        throw std::logic_error("The radii of the circles must be greater than 0");
+      }
+      if (pos1.x == pos2.x && pos1.y == pos2.y)
+      {
+        throw std::logic_error("The centers of the circles should not coincide");
+      }
+      if ((sqrt((pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y)) + r2) > r1)
+      {
+        throw std::logic_error("The smaller circle should lie completely inside the larger one");
+      }
+    }
+    Rubber(Rubber& other):
+      r1(other.r1), r2(other.r2), pos1(other.pos1), pos2(other.pos2)
+    {}
+    Rubber(Rubber&& other):
+      r1(std::move(other.r1)), r2(std::move(other.r2)), pos1(std::move(other.pos1)), pos2(std::move(other.pos2))
+    {}
+    Rubber& operator=(Rubber& other)
+    {
+      if (this != &other)
+      {
+        r1 = other.r1;
+        r2 = other.r2;
+        pos1 = other.pos1;
+        pos2 = other.pos2;
+      }
+      return * this;
+    }
+    Rubber& operator=(Rubber&& other)
+    {
+      if (this != &other)
+      {
+        r1 = other.r1;
+        r2 = other.r2;
+        pos1 = other.pos1;
+        pos2 = other.pos2;
+      }
+      return * this;
+    }
+    ~Rubber() override = default;
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t) override;
+    void move(double, double) override;
+    void scale(double k) override;
+  private:
+    double r1, r2;
+    point_t pos1, pos2;
   };
 }
 
@@ -94,4 +153,37 @@ void goltsov::Rectangle::scale(double k)
 {
   a.height *= k;
   a.width *= k;
+}
+
+double goltsov::Rubber::getArea() const
+{
+  return M_PI * (r1 * r1 - r2 * r2);
+}
+goltsov::rectangle_t goltsov::Rubber::getFrameRect() const
+{
+  return {r1 * 2, r1 * 2, pos1};
+}
+void goltsov::Rubber::move(goltsov::point_t newPos)
+{
+  double dx = pos1.x - pos2.x;
+  double dy = pos1.y - pos2.y;
+  pos2 = newPos;
+  pos1.x = pos2.x + dx;
+  pos1.y = pos2.y + dy;
+}
+void goltsov::Rubber::move(double dx, double dy)
+{
+  pos1.x += dx;
+  pos2.x += dx;
+  pos1.y += dy;
+  pos2.y += dy;
+}
+void goltsov::Rubber::scale(double k)
+{
+  r1 *= k;
+  r2 *= k;
+  double dx = pos1.x - pos2.x;
+  double dy = pos1.y - pos2.y;
+  pos1.x = pos2.x + k * dx;
+  pos1.y = pos2.y + k * dy;
 }
