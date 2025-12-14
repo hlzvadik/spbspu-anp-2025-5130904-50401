6e2efc035435decd50e99d03ec3a85e49bfb78b3
vadik <vadik@debian>
2025-12-14 23:01:16 +0300

    Some changes

--- goltsov.vadim/P5/main.cpp
+++ goltsov.vadim/P5/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+const double PI = std::acos(-1.0);
+
 namespace goltsov
 {
   struct point_t
@@ -16,18 +18,18 @@ namespace goltsov
   {
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(point_t) = 0;
-    virtual void move(double, double) = 0;
-    virtual void scale(double k) = 0;
+    virtual void move(const point_t) = 0;
+    virtual void move(const double, const double) = 0;
+    virtual void scale(const double k) = 0;
     virtual ~Shape() = default;
   };
   struct Rectangle : Shape
   {
-    Rectangle(double width, double height, point_t pos)
+    Rectangle(const double width, const double height, const point_t pos)
     {
       if (width <= 0 || height <= 0)
       {
-        throw std::logic_error("The width and height must be greater than zero.");
+        throw std::logic_error("The width and height must be greater than zero");
       }
       a.width = width;
       a.height = height;
@@ -58,15 +60,15 @@ namespace goltsov
     ~Rectangle() = default;
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(point_t) override;
-    void move(double, double) override;
-    void scale(double k) override;
+    void move(const point_t) override;
+    void move(const double, const double) override;
+    void scale(const double k) override;
   private:
       rectangle_t a;
   };
   struct Rubber : Shape
   {
-    Rubber(double r1, point_t pos1, double r2, point_t pos2):
+    Rubber(const double r1, const point_t pos1, const double r2, const point_t pos2):
       r1(r1), r2(r2), pos1(pos1), pos2(pos2)
     {
       if (r1 <= 0 || r2 <= 0)
@@ -82,13 +84,13 @@ namespace goltsov
         throw std::logic_error("The smaller circle should lie completely inside the larger one");
       }
     }
-    Rubber(Rubber& other):
+    Rubber(const Rubber& other):
       r1(other.r1), r2(other.r2), pos1(other.pos1), pos2(other.pos2)
     {}
     Rubber(Rubber&& other):
       r1(std::move(other.r1)), r2(std::move(other.r2)), pos1(std::move(other.pos1)), pos2(std::move(other.pos2))
     {}
-    Rubber& operator=(Rubber& other)
+    Rubber& operator=(const Rubber& other)
     {
       if (this != &other)
       {
@@ -113,20 +115,20 @@ namespace goltsov
     ~Rubber() = default;
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(point_t) override;
-    void move(double, double) override;
-    void scale(double k) override;
+    void move(const point_t) override;
+    void move(const double, const double) override;
+    void scale(const double k) override;
   private:
     double r1, r2;
     point_t pos1, pos2;
   };
-  point_t polygonCentroid(const point_t* mtx, int n)
+  point_t polygonCentroid(const point_t* mtx, const size_t n)
   {
     if (n < 3)
     {
         point_t center = {0, 0};
         if (n > 0) {
-            for (int i = 0; i < n; i++)
+            for (size_t i = 0; i < n; i++)
             {
                 center.x += mtx[i].x;
                 center.y += mtx[i].y;
@@ -139,9 +141,9 @@ namespace goltsov
     double area = 0.0;
     double centroid_x = 0.0;
     double centroid_y = 0.0;
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
     {
-        int j = (i + 1) % n;
+        size_t j = (i + 1) % n;
         double cross = (mtx[i].x * mtx[j].y - mtx[j].x * mtx[i].y);
         area += cross;
         centroid_x += (mtx[i].x + mtx[j].x) * cross;
@@ -156,18 +158,23 @@ namespace goltsov
   }
   struct Polygon : Shape
   {
-    Polygon(point_t* mtx, size_t n):
-      mtx(mtx), n(n), pos(polygonCentroid(mtx, n))
+    Polygon(const point_t* mtx, const size_t n):
+      n(n), pos(polygonCentroid(mtx, n))
     {
       if (n < 3)
       {
         throw std::logic_error("The polygon must have at least 3 vertices");
       }
+      this->mtx = new point_t[n];
+      for (size_t i = 0; i < n; ++i)
+      {
+        this->mtx[i] = mtx[i];
+      }
     }
-    Polygon(Polygon& other):
+    Polygon(const Polygon& other):
       n(other.n), pos(other.pos)
     {
-      mtx = new point_t[n];
+      mtx = new point_t[other.n];
       for (size_t i = 0; i < n; ++i)
       {
         mtx[i] = other.mtx[i];
@@ -179,17 +186,18 @@ namespace goltsov
       mtx = other.mtx;
       other.mtx = nullptr;
     }
-    Polygon& operator=(Polygon& other)
+    Polygon& operator=(const Polygon& other)
     {
       if (this != &other)
       {
+        delete[] mtx;
+        n = other.n;
+        pos = other.pos;
         mtx = new point_t[n];
         for (size_t i = 0; i < n; ++i)
         {
           mtx[i] = other.mtx[i];
         }
-        n = other.n;
-        pos = other.pos;
       }
       return * this;
     }
@@ -197,6 +205,7 @@ namespace goltsov
     {
       if (this != &other)
       {
+        delete[] mtx;
         mtx = other.mtx;
         other.mtx = nullptr;
         n = other.n;
@@ -210,9 +219,9 @@ namespace goltsov
     }
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(point_t) override;
-    void move(double, double) override;
-    void scale(double k) override;
+    void move(const point_t) override;
+    void move(const double, const double) override;
+    void scale(const double k) override;
   private:
     point_t* mtx = nullptr;
     size_t n;
@@ -224,10 +233,26 @@ int main()
 {
   using namespace goltsov;
   Rectangle a = {1, 5, {2, 3}};
-  Rectangle b = a;
-  b.scale(2);
-  std::cout << a.getArea() << ' ' << b.getArea() << '\n';
-  return 0;
+  double a1 = a.getArea();
+  rectangle_t a2 = a.getFrameRect();
+  a.move({1,1});
+  a.move(2,2);
+  a.scale(2.0);
+
+  Rubber b = {4.4, {1, 1}, 1.1, {1.1, 1.1}};
+  double b1 = b.getArea();
+  rectangle_t b2 = b.getFrameRect();
+  b.move({3,3});
+  b.move(3,3);
+  b.scale(2.5);
+
+  point_t mas[5] = {{0, 0}, {1, 0}, {2, 2}, {2, 3}, {1, 4}};
+  Polygon c = {mas, 5};
+  double c1 = c.getArea();
+  rectangle_t c2 = c.getFrameRect();
+  c.move({1, 1});
+  c.move(2,2);
+  c.scale(1.1);
 }
 
 double goltsov::Rectangle::getArea() const
@@ -238,16 +263,16 @@ goltsov::rectangle_t goltsov::Rectangle::getFrameRect() const
 {
   return a;
 }
-void goltsov::Rectangle::move(goltsov::point_t newPos)
+void goltsov::Rectangle::move(const goltsov::point_t newPos)
 {
   a.pos = newPos;
 }
-void goltsov::Rectangle::move(double dx, double dy)
+void goltsov::Rectangle::move(const double dx, const double dy)
 {
   a.pos.x += dx;
   a.pos.y += dy;
 }
-void goltsov::Rectangle::scale(double k)
+void goltsov::Rectangle::scale(const double k)
 {
   a.height *= k;
   a.width *= k;
@@ -255,13 +280,13 @@ void goltsov::Rectangle::scale(double k)
 
 double goltsov::Rubber::getArea() const
 {
-  return M_PI * (r1 * r1 - r2 * r2);
+  return PI * (r1 * r1 - r2 * r2);
 }
 goltsov::rectangle_t goltsov::Rubber::getFrameRect() const
 {
   return {r1 * 2, r1 * 2, pos1};
 }
-void goltsov::Rubber::move(goltsov::point_t newPos)
+void goltsov::Rubber::move(const goltsov::point_t newPos)
 {
   double dx = pos1.x - pos2.x;
   double dy = pos1.y - pos2.y;
@@ -269,14 +294,14 @@ void goltsov::Rubber::move(goltsov::point_t newPos)
   pos1.x = pos2.x + dx;
   pos1.y = pos2.y + dy;
 }
-void goltsov::Rubber::move(double dx, double dy)
+void goltsov::Rubber::move(const double dx, const double dy)
 {
   pos1.x += dx;
   pos2.x += dx;
   pos1.y += dy;
   pos2.y += dy;
 }
-void goltsov::Rubber::scale(double k)
+void goltsov::Rubber::scale(const double k)
 {
   r1 *= k;
   r2 *= k;
@@ -290,9 +315,9 @@ double goltsov::Polygon::getArea() const
 {
     if (n < 3) return 0.0;
     double area = 0.0;
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
     {
-        int j = (i + 1) % n;
+        size_t j = (i + 1) % n;
         area += mtx[i].x * mtx[j].y - mtx[j].x * mtx[i].y;
     }
     return fabs(area) * 0.5;
@@ -308,11 +333,11 @@ goltsov::rectangle_t goltsov::Polygon::getFrameRect() const
     max_x = (max_x < mtx[i].x ? mtx[i].x : max_x);
     min_x = (min_x > mtx[i].x ? mtx[i].x : min_x);
     max_y = (max_y < mtx[i].y ? mtx[i].y : max_y);
-    min_x = (min_y > mtx[i].y ? mtx[i].y : min_y);
+    min_y = (min_y > mtx[i].y ? mtx[i].y : min_y);
   }
   return {max_x - min_x, max_y - min_y, {(max_x + min_x)/2, (max_y + min_y)/2}};
 }
-void goltsov::Polygon::move(goltsov::point_t newPos)
+void goltsov::Polygon::move(const goltsov::point_t newPos)
 {
   double dx = newPos.x - pos.x;
   double dy = newPos.y - pos.y;
@@ -323,7 +348,7 @@ void goltsov::Polygon::move(goltsov::point_t newPos)
   }
   pos = newPos;
 }
-void goltsov::Polygon::move(double dx, double dy)
+void goltsov::Polygon::move(const double dx, const double dy)
 {
   for (size_t i = 0; i < n; ++i)
   {
@@ -333,7 +358,7 @@ void goltsov::Polygon::move(double dx, double dy)
   pos.x += dx;
   pos.y += dy;
 }
-void goltsov::Polygon::scale(double k)
+void goltsov::Polygon::scale(const double k)
 {
   for (size_t i = 0; i < n; ++i)
   {
